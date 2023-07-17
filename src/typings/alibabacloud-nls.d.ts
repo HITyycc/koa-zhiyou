7177140a9994d838e03a21b788bedf5c1b659cf3
1119110966@qq.com <1119110966@qq.com>
2023-07-17 23:49:57 +0800

    feat: speech

--- src/typings/alibabacloud-nls.d.ts
+++ src/typings/alibabacloud-nls.d.ts
@@ -2,24 +2,25 @@
 declare module "alibabacloud-nls" {
     interface SpeechTranscriptionConfig {
         url: string
-        token: string
+        token: Object
         appkey: string
     }
 
     interface StartParams {
         format: string,
         sample_rate: number,
-        enable_intermediate_result: true,
-        enable_inverse_text_normalization: true
+        enable_intermediate_result?: boolean,
+        enable_inverse_text_normalization?: boolean,
+        enable_punctuation_prediction?: boolean
     }
 
     type event = "started" | "changed" | "completed" | "closed" | "failed" | "begin" | "end"
     
     class SpeechTranscription {
         constructor(config: SpeechTranscriptionConfig)
-        on: (which: event, handler: () => void) => void
+        on: (which: event, handler: (msg: Object) => void) => void
         start: (param: StartParams, enablePing: boolean, pingInterval: number) => Promise<string>
-        close: (param: Object) => Promise<object>
+        close: (param?: Object) => Promise<object>
         shutdown: () => void
         sendAudio: (data: any) => void
     }
